d42f618582b0781a5d37442c8c45732ab219d04d
5maciek <smaciek1@op.pl>
2020-05-21 15:12:50 +0200

    Updates

---
+++ precache-manifest.bf8240681db4c435b84e9de5db4f6cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5080b479cbc5c4c76bc19d53696ccbd8",
+    "url": "/react-mastermind-game/index.html"
+  },
+  {
+    "revision": "ce165bd15c53e79c00f3",
+    "url": "/react-mastermind-game/static/css/main.9148254e.chunk.css"
+  },
+  {
+    "revision": "8a691c4e1152005289d6",
+    "url": "/react-mastermind-game/static/js/2.9b209c57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mastermind-game/static/js/2.9b209c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce165bd15c53e79c00f3",
+    "url": "/react-mastermind-game/static/js/main.4d037833.chunk.js"
+  },
+  {
+    "revision": "7ec1465f85679c064f8c",
+    "url": "/react-mastermind-game/static/js/runtime-main.72e27555.js"
+  }
+]);
